7ade0109c2f29a24b20435b41af1af9a2ad9baaf
Adam Dimmick <66253578+awdimmick@users.noreply.github.com>
2023-09-25 20:13:21 +0100

    Added decodeHighNibble and decodeLowNibble functions

--- main.py
+++ main.py
@@ -1,52 +1,8 @@
-def on_received_number(receivedNumber):
-    global receiveBuffer
-    receiveBuffer.append(receivedNumber)
-    if len(receiveBuffer) == 8:
-        processReceiveBuffer()
-        receiveBuffer = []
-radio.on_received_number(on_received_number)
-
-def showBufferOnLED():
-    for index in range(4):
-        led.plot_brightness(index, 0, 64 + 191 * sendBuffer[index])
-        led.plot_brightness(index, 3, 64 + 191 * sendBuffer[4 + index])
-
-def processReceiveBuffer():
-    global screenInUse
-    screenInUse = 1
-    basic.show_number(processBitArray(receiveBuffer))
-    basic.pause(200)
-    screenInUse = 0
-
-def on_button_pressed_a():
-    sendBuffer[currentBit] = 0
-    incrementCurrentBit()
-input.on_button_pressed(Button.A, on_button_pressed_a)
-
-def transmitSendBuffer():
-    global sendBitIndex, currentBit, sendBuffer
-    sendBitIndex = len(sendBuffer) - 1
-    while sendBitIndex >= 0:
-        radio.send_number(sendBuffer[sendBitIndex])
-        basic.pause(200)
-        sendBitIndex += -1
-    currentBit = 0
-    sendBuffer = []
-
-def processBitArray(bits: List[any]):
-    global byteValue
-    byteValue = 0
-    index = 0
-    while index <= len(bits) - 1:
-        byteValue = byteValue + bits[index] * 2 ** index
-        index += 1
-    return byteValue
-
-def showCurrentBitOnLED():
-    for index in range(4):
-        led.unplot(index, 1)
-        led.unplot(index, 4)
-    led.plot(currentBit % 4, 1 + Math.idiv(currentBit, 4) * 3)
+# processReceiveBuffer() is the function that you need to modify.
+# 
+# This function is called when a byte of data is received.
+# 
+# Setting screenInUse to 1 pauses the background process that shows the current state of the sendBuffer on the micro:bit's LEDs. Setting it back to 0 will resume the display.
 
 def on_button_pressed_ab():
     global screenInUse
@@ -77,6 +33,68 @@ def on_button_pressed_b():
     incrementCurrentBit()
 input.on_button_pressed(Button.B, on_button_pressed_b)
 
+def showBufferOnLED():
+    for index in range(4):
+        led.plot_brightness(index, 0, 64 + 191 * sendBuffer[index])
+        led.plot_brightness(index, 3, 64 + 191 * sendBuffer[4 + index])
+"""
+
+processReceiveBuffer() is the function that you should modify to determine what your micro:bit should do once a byte of data has been received. 
+
+You could write your handling code directly in here, or create new functions to call.
+
+Setting screenInUse to 1 "takes over" the screen and disables the background process that shows the state of the sendBuffer on the micro:bit's LED display. Setting it back to 0 releases the screen once again.
+
+"""
+def processReceiveBuffer():
+    global screenInUse
+    screenInUse = 1
+    basic.show_number(decodeBitArray(receiveBuffer))
+    basic.pause(200)
+    basic.clear_screen()
+    screenInUse = 0
+def decodeBitArray(bits: List[any]):
+    global byteValue
+    byteValue = 0
+    index2 = 0
+    while index2 <= len(bits) - 1:
+        byteValue = byteValue + bits[index2] * 2 ** (7 - index2)
+        index2 += 1
+    return byteValue
+
+def on_button_pressed_a():
+    sendBuffer[currentBit] = 0
+    incrementCurrentBit()
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def decodeHighNibble(bits2: List[number]):
+    global nibbleValue
+    nibbleValue = 0
+    for index3 in range(4):
+        nibbleValue = nibbleValue + bits2[index3] * 2 ** (3 - index3)
+    return nibbleValue
+def transmitSendBuffer():
+    global currentBit, sendBuffer
+    for index4 in range(8):
+        radio.send_number(sendBuffer[index4])
+        basic.pause(200)
+    currentBit = 0
+    sendBuffer = []
+
+def on_received_number(receivedNumber):
+    global receiveBuffer
+    receiveBuffer.append(receivedNumber)
+    if len(receiveBuffer) == 8:
+        processReceiveBuffer()
+        receiveBuffer = []
+radio.on_received_number(on_received_number)
+
+def showCurrentBitOnLED():
+    for index32 in range(4):
+        led.unplot(index32, 1)
+        led.unplot(index32, 4)
+    led.plot(currentBit % 4, 1 + Math.idiv(currentBit, 4) * 3)
+
 def on_gesture_shake():
     global screenInUse, currentBit, sendBuffer
     screenInUse = 0
@@ -84,17 +102,31 @@ def on_gesture_shake():
     sendBuffer = []
 input.on_gesture(Gesture.SHAKE, on_gesture_shake)
 
+def decodeLowNibble(bits3: List[number]):
+    global nibbleValue
+    nibbleValue = 0
+    for index5 in range(4):
+        nibbleValue = nibbleValue + bits3[index5 + 4] * 2 ** (3 - index5)
+    return nibbleValue
 def incrementCurrentBit():
     global currentBit
     currentBit = currentBit + 1
     if currentBit == 8:
         currentBit = 0
+"""
+
+Each micro:bit in your group must be using the same radio group value.
+
+Update this value before downloading your code and loading onto your micro:bits!
+
+"""
+nibbleValue = 0
 byteValue = 0
-sendBitIndex = 0
 receiveBuffer: List[number] = []
 sendBuffer: List[number] = []
 currentBit = 0
 screenInUse = 0
+radio.set_group(0)
 screenInUse = 0
 currentBit = 0
 sendBuffer = []
--- main.ts
+++ main.ts
@@ -1,10 +1,8 @@
-/**
- * This is the function that you need to modify. 
- * 
- * This function is called when a byte of data is received.
- * 
- * Setting screenInUse to 1 pauses the background process that shows the current state of the sendBuffer on the micro:bit's LEDs. Setting it back to 0 will resume the display.
- */
+// processReceiveBuffer() is the function that you need to modify.
+// 
+// This function is called when a byte of data is received.
+// 
+// Setting screenInUse to 1 pauses the background process that shows the current state of the sendBuffer on the micro:bit's LEDs. Setting it back to 0 will resume the display.
 input.onButtonPressed(Button.AB, function () {
     if (sendBuffer.length == 8) {
         screenInUse = 1
@@ -38,22 +36,42 @@ function showBufferOnLED () {
         led.plotBrightness(index, 3, 64 + 191 * sendBuffer[4 + index])
     }
 }
+/**
+ * processReceiveBuffer() is the function that you should modify to determine what your micro:bit should do once a byte of data has been received. 
+ * 
+ * You could write your handling code directly in here, or create new functions to call.
+ * 
+ * Setting screenInUse to 1 "takes over" the screen and disables the background process that shows the state of the sendBuffer on the micro:bit's LED display. Setting it back to 0 releases the screen once again.
+ */
 function processReceiveBuffer () {
     screenInUse = 1
-    basic.showNumber(processBitArray(receiveBuffer))
+    basic.showNumber(decodeBitArray(receiveBuffer))
     basic.pause(200)
+    basic.clearScreen()
     screenInUse = 0
+}
+function decodeBitArray (bits: any[]) {
+    byteValue = 0
+    for (let index = 0; index <= bits.length - 1; index++) {
+        byteValue = byteValue + bits[index] * 2 ** (7 - index)
+    }
+    return byteValue
 }
 input.onButtonPressed(Button.A, function () {
     sendBuffer[currentBit] = 0
     incrementCurrentBit()
 })
+function decodeHighNibble (bits: number[]) {
+    nibbleValue = 0
+    for (let index = 0; index <= 3; index++) {
+        nibbleValue = nibbleValue + bits[index] * 2 ** (3 - index)
+    }
+    return nibbleValue
+}
 function transmitSendBuffer () {
-    sendBitIndex = sendBuffer.length - 1
-    while (sendBitIndex >= 0) {
-        radio.sendNumber(sendBuffer[sendBitIndex])
+    for (let index = 0; index <= 7; index++) {
+        radio.sendNumber(sendBuffer[index])
         basic.pause(200)
-        sendBitIndex += -1
     }
     currentBit = 0
     sendBuffer = []
@@ -65,17 +83,10 @@ radio.onReceivedNumber(function (receivedNumber) {
         receiveBuffer = []
     }
 })
-function processBitArray (bits: any[]) {
-    byteValue = 0
-    for (let index = 0; index <= bits.length - 1; index++) {
-        byteValue = byteValue + bits[index] * 2 ** index
-    }
-    return byteValue
-}
 function showCurrentBitOnLED () {
-    for (let index = 0; index <= 3; index++) {
-        led.unplot(index, 1)
-        led.unplot(index, 4)
+    for (let index3 = 0; index3 <= 3; index3++) {
+        led.unplot(index3, 1)
+        led.unplot(index3, 4)
     }
     led.plot(currentBit % 4, 1 + Math.idiv(currentBit, 4) * 3)
 }
@@ -84,18 +95,31 @@ input.onGesture(Gesture.Shake, function () {
     currentBit = 0
     sendBuffer = []
 })
+function decodeLowNibble (bits: number[]) {
+    nibbleValue = 0
+    for (let index = 0; index <= 3; index++) {
+        nibbleValue = nibbleValue + bits[index + 4] * 2 ** (3 - index)
+    }
+    return nibbleValue
+}
 function incrementCurrentBit () {
     currentBit = currentBit + 1
     if (currentBit == 8) {
         currentBit = 0
     }
 }
+/**
+ * Each micro:bit in your group must be using the same radio group value.
+ * 
+ * Update this value before downloading your code and loading onto your micro:bits!
+ */
+let nibbleValue = 0
 let byteValue = 0
-let sendBitIndex = 0
 let receiveBuffer: number[] = []
 let sendBuffer: number[] = []
 let currentBit = 0
 let screenInUse = 0
+radio.setGroup(0)
 screenInUse = 0
 currentBit = 0
 sendBuffer = []
